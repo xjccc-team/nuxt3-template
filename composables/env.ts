423aa5c2eb06b231e9cbfced3bf251172b56b77a
xjccc <546534045@qq.com>
2024-04-28 11:30:37 +0800

    update: eslint config

--- composables/env.ts
+++ composables/env.ts
@@ -1,3 +1,3 @@
 // 环境
 const checkDev = (arr:string[]) => arr.some(item => ~window.location.host.indexOf(item))
-export const DEBUG = process.server ? process.env.NODE_ENV === 'development' : checkDev(['local','192','-test'])
+export const DEBUG = import.meta.server ? process.env.NODE_ENV === 'development' : checkDev(['local','192','-test'])
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,72 +1,22 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import withNuxt from './.nuxt/eslint.config.mjs'
 
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname
-})
-
-export default [
-  {
-    ignores: [
-      'node_modules/*',
-      'node-server/*',
-      'dist/*',
-      '.output',
-      '.data',
-      '.nuxt'
+export default withNuxt({
+  files: ['**/*.ts', '**/*.vue'],
+  rules: {
+    'vue/multi-word-component-names': 0,
+    // 禁用未声明变量，除非他们在/* global */注释中被提到
+    'no-undef': 0,
+    'vue/no-v-model-argument': 0,
+    'vue/no-v-html': 0,
+    camelcase: 0,
+    complexity: 0,
+    'no-mixed-operators': 0,
+    'vue/valid-v-for': 0,
+    'max-len': 0,
+    'no-empty-function': 0,
+    'object-property-newline': [
+      'error',
+      { allowAllPropertiesOnSameLine: true }
     ]
-  },
-  ...compat.config({
-    env: {
-      browser: true,
-      node: true,
-      es2021: true,
-      'vue/setup-compiler-macros': true
-    },
-    extends: [
-      'plugin:vue/vue3-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:nuxt/recommended'
-    ],
-    overrides: [],
-    parser: 'vue-eslint-parser',
-    parserOptions: {
-      parser: '@typescript-eslint/parser',
-      ecmaVersion: 'latest',
-      sourceType: 'module'
-    },
-    plugins: ['vue', '@typescript-eslint'],
-    rules: {
-      'vue/multi-word-component-names': 0,
-      // 禁止出现未使用过的变量
-      'no-unused-vars': [
-        0,
-        {
-          vars: 'all',
-          args: 'none'
-        }
-      ],
-      // 启用 @typescript-eslint 的 no-unused-vars 规则
-      '@typescript-eslint/no-unused-vars': [
-        0,
-        {
-          vars: 'all',
-          args: 'none'
-        }
-      ],
-      // 禁用未声明变量，除非他们在/* global */注释中被提到
-      'no-undef': 0,
-      'vue/no-v-model-argument': 0,
-      'vue/no-v-html': 0,
-      camelcase: 0,
-      complexity: 0,
-      'no-mixed-operators': 0,
-      'vue/valid-v-for': 0,
-      'max-len': 0
-    }
-  })
-]
+  }
+})
