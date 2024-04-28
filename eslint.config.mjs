adcef654d533768ddfee90225ec5011db3e58d56
xjccc <546534045@qq.com>
2024-04-28 15:27:32 +0800

    update: eslint-plugin-vue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,34 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
+import pluginVue from 'eslint-plugin-vue'
 
-export default withNuxt({
-  files: ['**/*.ts', '**/*.vue'],
-  rules: {
-    'vue/multi-word-component-names': 0,
-    // 禁用未声明变量，除非他们在/* global */注释中被提到
-    'no-undef': 0,
-    'vue/no-v-model-argument': 0,
-    'vue/no-v-html': 0,
-    camelcase: 0,
-    complexity: 0,
-    'no-mixed-operators': 0,
-    'vue/valid-v-for': 0,
-    'max-len': 0,
-    'no-empty-function': 0,
-    'object-property-newline': [
-      'error',
-      { allowAllPropertiesOnSameLine: true }
-    ]
+export default withNuxt([
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.ts', '**/*.vue'],
+    rules: {
+      // 启用 @typescript-eslint 的 no-unused-vars 规则
+      '@typescript-eslint/no-unused-vars': [
+        0,
+        {
+          vars: 'all',
+          args: 'none'
+        }
+      ],
+      'vue/multi-word-component-names': 0,
+      // 禁用未声明变量，除非他们在/* global */注释中被提到
+      'no-undef': 0,
+      'vue/no-v-model-argument': 0,
+      'vue/no-v-html': 0,
+      camelcase: 0,
+      complexity: 0,
+      'no-mixed-operators': 0,
+      'vue/valid-v-for': 0,
+      'max-len': 0,
+      'no-empty-function': 0,
+      'object-property-newline': [
+        'error',
+        { allowAllPropertiesOnSameLine: true }
+      ]
+    }
   }
-})
+])
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="container">
+  <div
+    class="container"
+    :name="1"
+  >
     首页
   </div>
 </template>
 <script setup lang="ts">
+const a = ref(0)
 </script>
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
