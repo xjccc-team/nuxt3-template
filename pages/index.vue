877b4c3ae102387c2d057780fbb86b5d6366bacf
xjccc <546534045@qq.com>
2024-04-29 12:14:25 +0800

    feat: prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,35 @@
 <template>
   <div
     class="container"
-    style="color: red;;"
+    style="color: red"
   >
     首页
   </div>
 </template>
 <script setup lang="ts">
-const a = ref(0)
+  const a = ref(0);
 </script>
 <style lang="less" scoped>
-.ccss{
-  color: red
-}
-a { top: calc(1px + 2px); }
+  .ccss {
+    color: red;
+  }
+  a {
+    top: calc(1px + 2px);
+  }
 
-.a::v-deep{
-  color: yellow;
-}
+  .a::v-deep {
+    color: yellow;
+  }
 
-.a{
-  color: red;
-}
+  .a {
+    color: red
+  }
 
-text * {
-  font-size: var(100%);
-}
+  text * {
+    font-size: var(100%);
+  }
 
-.bg{
-  background-color:url("https://img.alicdn.com/tfs/TB1.rjJQFXXXXXKXpXXXXXXXXXX-200-200.png");
-}
+  .bg {
+    background-color: url('https://img.alicdn.com/tfs/TB1.rjJQFXXXXXKXpXXXXXXXXXX-200-200.png');
+  }
 </style>
