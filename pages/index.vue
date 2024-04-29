c0111daac075e329e4df96200326e9fea2d22e47
xjccc <546534045@qq.com>
2024-04-29 10:50:49 +0800

    feat: prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container">
+  <div
+    class="container"
+    style="color: red;;"
+  >
     首页
   </div>
 </template>
@@ -8,9 +11,23 @@ const a = ref(0)
 </script>
 <style lang="less" scoped>
 .ccss{
+  color: red
+}
+a { top: calc(1px + 2px); }
+
+.a::v-deep{
+  color: yellow;
+}
+
+.a{
   color: red;
 }
+
+text * {
+  font-size: var(100%);
+}
+
 .bg{
-  background-color:url(https://img.alicdn.com/tfs/TB1.rjJQFXXXXXKXpXXXXXXXXXX-200-200.png);
+  background-color:url("https://img.alicdn.com/tfs/TB1.rjJQFXXXXXKXpXXXXXXXXXX-200-200.png");
 }
 </style>
